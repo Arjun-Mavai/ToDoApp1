8add94ce52379dd861f4176e8eb49ed99e3693ab
Arjun Singh Gurjar <yuyureka212@gmail.com>
2023-08-01 00:43:36 +0530

    Update TodoItem.js
    added icon feature

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,17 @@
-import React from 'react';
+ import React from 'react';
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo   }) => {
   return (
     <div>
       <input type="checkbox" checked={todo.completed} />
-      <span>{todo.text}</span>
+      
+      <span>{todo.text}</span> 
+     
+      
+      
+      <span>{todo.name}</span>  <span> </span>
+      <i class="fa-solid fa-trash-can  fa-lg icon-delete"  ></i>
+      {'\n'}
     </div>
   );
 };
