a2db83a6e169b07852a214b33b18093b6916aa96
Arjun Singh Gurjar <yuyureka212@gmail.com>
2023-08-01 00:42:26 +0530

    Update App.js
    after adding remove all feature and added the button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React, { useState } from 'react';
-import TodoList from './components/TodoList';
-import AddTodoForm from './components/AddTodoForm';
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import AddTodoForm from "./components/AddTodoForm";
+import "./App.css";
+
 
 const App = () => {
   const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: false },
-    { id: 2, text: 'Build Todo App', completed: true },
+    { id: 1, text: "Learn React", completed: false  },
+    { id: 2, text: "Build Todo App", completed: true  },
   ]);
 
   const addTodo = (text) => {
-    const newTodo = { id: Date.now(), text, completed: false };
-    setTodos([...todos, newTodo]);
+    if (text.trim() !== "") {
+      // execute only when lhs != rhs , like saying text is not empty then run it, checking empty string not running the code , empty != empty '' : condition false so won't run , if text sth it will run
+
+      const newTodo = {
+        id: Date.now(),
+        text,
+        completed: false,
+        
+      };
+      setTodos([...todos, newTodo]);
+    }
   };
 
+  
+  const removeAllTodos = () => {
+    setTodos([]);
+  };
+
+   // inside button i was using onChange which was creating problem , not letting the code working properly
+
   return (
-    <div>
+    <div className="App-header">
+      <i className="fa-solid fa-bell fa-shake fa-lg"></i>
       <h1>My Todo App</h1>
-      <TodoList todos={todos} />
+      <button onClick={removeAllTodos}>Remove all todos</button> 
+      <TodoList todos={todos}  removeAllTodos = {removeAllTodos} /> <br />
       <AddTodoForm addTodo={addTodo} />
     </div>
   );
